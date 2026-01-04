0570a7123c7af9679aca340ae9cf8a60c120dbc7
Masud-Ndatsu <masudndatsu@gmail.com>
2026-01-04 10:34:23 +0100

    fix: converted the png logos to svg

--- nebula-app/src/lib/images/logos.ts
+++ nebula-app/src/lib/images/logos.ts
@@ -1,9 +1,9 @@
 // Logo images
 import type { StaticImageData } from "next/image";
-import nebulaIcon from "./logos/nebula-icon-512.png";
-import nebula from "./logos/nebula.png";
+import nebulaIcon from "./logos/nebula.svg";
+import nebula from "./logos/nebula.svg";
 import nebulaSvg from "./logos/nebula.svg";
-import nebulaLogo from "./logos/nebula_logo.png";
+import nebulaLogo from "./logos/nebula_logo.svg";
 
 export const logos = {
   nebulaIcon,
