37857255db238be645ee391a476e205793f567c0
Masud-Ndatsu <masudndatsu@gmail.com>
2026-03-29 12:26:47 +0100

    chore: made some changes

--- packages/database/src/config/env.ts
+++ packages/database/src/config/env.ts
@@ -3,16 +3,8 @@ import { z } from "zod";
 
 dotenv.config();
 
-
-const envSchema = z.object({
-    DATABASE_URL: z.string().min(1),
-});
-
-const _env = envSchema.safeParse(process.env);
-
-if (!_env.success) {
-    console.error("❌ Invalid environment variables:", _env.error.format());
-    process.exit(1);
+const _env = {
+    DATABASE_URL: process.env.DATABASE_URL,
 }
 
-export const env = _env.data;
+export const env = _env;
